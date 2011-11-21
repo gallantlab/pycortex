50dc37b093ab5f49289c88478db0b8812a50ff63
james <james@arcturus.(none)>
2011-11-20 22:22:48 -0800

    Well, I did get the surface-drawing working, but it's worthlessly slow. However, I realized that the texturing means that I can make the images in inkscape, apply them to these flatmaps trivially. Inkscape to the rescue!

--- drawsurf.py
+++ drawsurf.py
@@ -1,5 +1,9 @@
+import numpy as np
+from matplotlib.pyplot import imread
+import cairo
+
 try:
-    from traits.api import HasTraits, Instance, Array, Bool, Dict, Range, Any, Color,Enum, Callable, on_trait_change
+    from traits.api import HasTraits, Instance, Array, Bool, Dict, Range, Any, Color,Enum, Str, Callable, on_trait_change
     from traitsui.api import View, Item, HGroup, Group, ImageEnumEditor, ColorEditor
 
     from tvtk.api import tvtk
@@ -9,8 +13,9 @@ try:
     from mayavi.core.ui import lut_manager
     from mayavi.core.api import PipelineBase, Source, Filter, Module
     from mayavi.core.ui.api import SceneEditor, MlabSceneModel, MayaviScene
+    from mayavi.sources.array_source import ArraySource
 except ImportError:
-    from enthought.traits.api import HasTraits, Instance, Array, Bool, Dict, Any, Range, Color,Enum, Callable, on_trait_change
+    from enthought.traits.api import HasTraits, Instance, Array, Bool, Dict, Any, Range, Color,Enum, Str, Callable, on_trait_change
     from enthought.traits.ui.api import View, Item, HGroup, Group, ImageEnumEditor, ColorEditor, Handler
 
     from enthought.tvtk.api import tvtk
@@ -20,7 +25,7 @@ except ImportError:
     from enthought.mayavi.core.ui import lut_manager
     from enthought.mayavi.core.api import PipelineBase, Source, Filter, Module
     from enthought.mayavi.core.ui.api import SceneEditor, MlabSceneModel, MayaviScene
-
+    from enthought.mayavi.sources.array_source import ArraySource
 
 import view
 
@@ -45,36 +50,94 @@ class MouseDownScene(MayaviScene):
         if self.move:
             return super(MouseDownScene, self).OnButtonUp(evt)
 
-class PickPoints(view.Mixer):
+clear_white_black = np.tile(0, [85, 4]), np.tile(255, [86, 4]), np.tile(0, [85, 4]),
+clear_white_black[-1][:,-1] = 255
+clear_white_black = np.vstack(clear_white_black)
+
+class DrawOnTex(view.Mixer):
+    texpath = Str
+    tex = Instance(ArraySource)
+
+    csurf = Instance(cairo.ImageSurface, (cairo.FORMAT_ARGB32, 2048, 1024))
+    cctx = Instance(cairo.Context)
+
+    def _cctx_default(self):
+        ctx = cairo.Context(self.csurf)
+        ctx.set_line_width(5)
+        return ctx
+
+    def _data_src_default(self):
+        src = super(DrawOnTex, self)._data_src_default()
+        #Generate texture coordinates from the flatmap coordinates
+        pts = self.points(1)
+        pts -= pts.min(0)
+        pts /= pts.max(0)
+        src.data.point_data.t_coords = pts[:,[0,2]]
+        return src
+
+    def _surf_default(self):
+        surf = super(DrawOnTex, self)._surf_default()
+        surf.actor.enable_texture = True
+        surf.actor.texture_source_object = self.tex
+        surf.actor.texture.interpolate = True
+        surf.actor.texture.repeat = False
+        surf.actor.texture.lookup_table = tvtk.LookupTable(
+            table=clear_white_black, range=(-1,1))
+        return surf
+    
+
+    def _tex_default(self):
+        return ArraySource(scalar_data=np.zeros((2048,1024)))
+    
+    def _texpath_changed(self):
+        self.tex.scalar_data = np.load(self.texpath)
+        
     def _start_picker(self):
-        self.blah = []
+        self.last = None
         def callback(widget, evt):
             self.figure.scene_editor.control.SetFocusFromKbd()
-            pos = widget.GetEventPosition()
             if widget.GetShiftKey() == 1:
                 self.figure.scene_editor.move = False
             else:
                 self.figure.scene_editor.move = True
 
             if self.figure.scene_editor.mousedown:
+                pos = widget.GetEventPosition()
                 self.figure.picker.pointpicker.pick(
                     (pos[0], pos[1], 0), 
                     self.figure.mayavi_scene.scene.renderer)
+            else:
+                if self.last is not None:
+                    #Finished drawing, time to draw the stroke
+                    self._draw()
+                self.last = None
                 
         self.figure.interactor.add_observer("MouseMoveEvent", callback)
         picker = self.figure.mayavi_scene.on_mouse_pick(self._pick)
-        picker.tolerance = 0.01
+        picker.tolerance = 0.005
         
     @on_trait_change("figure.activated")
     def _start(self):
-        super(PickPoints, self)._start()
+        super(DrawOnTex, self)._start()
+        self.surf
         self.figure.scene_editor.move = True
-        self.figure.interactor.interactor_style = tvtk.InteractorStyleTerrain()
         self._start_picker()
 
     def _pick(self, picker):
         if picker.point_id != -1:
-            self.data_src.mlab_source.scalars[picker.point_id] = 0
+            u, v = self.data_src.data.point_data.t_coords[picker.point_id]
+            if self.last is not None:
+                self.cctx.line_to(u*2048,v*1024)
+            else:
+                self.cctx.move_to(u*2048,v*1024)
+            self.last = u, v
+    
+    def _draw(self):
+        print "Finalizing stroke"
+        self.cctx.stroke()
+        data = np.frombuffer(self.csurf.get_data(), np.uint8).reshape(1024, 2048, 4)
+        self.tex.scalar_data = (data[...,-1] == 255).astype(int).T
+        self.surf.update_pipeline()
     
     view = View(
         HGroup(
@@ -97,3 +160,11 @@ def show(subject, types=('inflated',), hemisphere="both"):
     m = PickPoints(points=interp, polys=polys)
     m.edit_traits()
     return m
+
+if __name__ == "__main__":
+    import cPickle
+    from scipy.interpolate import interp1d
+    x, y, polys = cPickle.load(open("../../Dropbox/mesh.pkl"))
+    interp = interp1d(x, y)
+    m = DrawOnTex(points=interp, polys=polys)
+    m.edit_traits()
