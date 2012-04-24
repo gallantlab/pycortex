9c48a5d924936167b33f6f325e3d721dc161507d
james <james@arcturus.(none)>
2012-04-24 11:37:47 -0700

    I'm boggled, everything worked until I rearranged the files a bit... wtf

--- webgl_view/resources/js/surfload.js
+++ webgl_view/resources/js/surfload.js
@@ -7,7 +7,7 @@ THREE.BinSurfLoader = function () {};
 THREE.BinSurfLoader.prototype = new THREE.Loader();
 THREE.BinSurfLoader.prototype.constructor = THREE.BinSurfLoader;
 
-THREE.BinSurfLoader.prototype.load = function ( url, callback ) {
+THREE.BinSurfLoader.prototype.load = function ( url, post, callback ) {
     var that = this;
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
@@ -19,18 +19,18 @@ THREE.BinSurfLoader.prototype.load = function ( url, callback ) {
             }
         }
     };
-    xhr.open( "GET", url, true );
-    xhr.responseType = 'arraybuffer'
-    xhr.send( null );
-};
-
-function makeVector(data) {
-    var output = [];
-    for (var i=0; i < data.length; i+=3) {
-        output.push(new THREE.Vector3(data[i], data[i+1], data[i+2]))
+    if (post) {
+        var params = "types="+post+"&";
+        xhr.open( "POST", url, true );
+        xhr.responseType = 'arraybuffer';
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhr.send( params );
+    } else {
+        xhr.open( "GET", url, true );
+        xhr.responseType = 'arraybuffer';
+        xhr.send( null );
     }
-    return output
-}
+};
 
 THREE.BinSurfLoader.prototype.parse = function ( data ) {
     var idata = new Uint32Array(data);
@@ -38,31 +38,67 @@ THREE.BinSurfLoader.prototype.parse = function ( data ) {
     var numsurfs = idata[0];
     var ptlen = idata[1];
     var attrib = {};
-    var pts = fdata.subarray(2,ptlen+2);
 
     var geometry = new THREE.Geometry();
+
+    var pts = fdata.subarray(2,ptlen+2);
     for (var i=0; i<ptlen; i+=3) {
         geometry.vertices.push(new THREE.Vector3(pts[i], pts[i+1], pts[i+2]));
     }
+    console.log("Got points");
 
+    var ptdat, verts;
     for (var i = 1; i < numsurfs-1; i++) {
-        var ptdat = fdata.subarray(i*ptlen+2, (i+1)*ptlen+2);
-        geometry.morphTargets.push({name:"surf"+(i-1), vertices:makeVector(ptdat)})
+        verts = [];
+        ptdat = fdata.subarray(i*ptlen+2, (i+1)*ptlen+2);
+        for (var i=0; i < ptdat.length; i+=3)
+            verts.push(new THREE.Vector3(ptdat[i], ptdat[i+1], ptdat[i+2]));
+        geometry.morphTargets.push({name:"surf"+(i-1), vertices:verts});
     }
+    console.log("Got morphs");
 
     var flat = fdata.subarray((numsurfs-1)*ptlen+2, numsurfs*ptlen+2);
-    geometry.morphTargets.push({name:"flat", vertices:makeVector(flat)})
+    var min = [1000,1000], max = [0,0], verts = [], pt;
+    for (var i=0; i < flat.length; i+=3) {
+        pt = new THREE.Vector3(flat[i], flat[i+1], flat[i+2]);
+        verts.push(pt);
+        if (min[0] < pt.x) min[0] = pt.x;
+        else if (max[0] > pt.x) max[0] = pt.x;
+        if (min[1] < pt.z) min[1] = pt.z;
+        else if (max[1] > pt.z) max[0] = pt.z;
+    }
+    geometry.morphTargets.push({name:"flat", vertices:verts});
+    console.log("Got flats");
 
     var polys = idata.subarray(numsurfs*ptlen+2);
     for (var i=0; i < polys.length; i+=3) {
-        geometry.faces.push(new THREE.Face3(polys[i], polys[i+1], polys[i+2]))
+        geometry.faces.push(new THREE.Face3(polys[i], polys[i+1], polys[i+2]));
     }
+    console.log("Got faces");
+
+    
+    //Generate the UV coordinates
+    var u, v, face, tcoords, n, names = ["a","b","c"];
+    for (var i=0; i < geometry.faces.length; i++) {
+        face = geometry.faces[i];
+        tcoords = [];
+        for (var j=0; j < 3; j++) {
+            n = names[j];
+            u = (verts[face[n]].x - min[0]) / (max[0] - min[0]);
+            v = (verts[face[n]].z - min[1]) / (max[1] - min[1]);
+            tcoords.push(new THREE.UV(u, v));
+        }
+        geometry.faceVertexUvs.push(tcoords);
+    }
+    console.log("Got UVs");
+
     
     geometry.computeCentroids();
     geometry.computeFaceNormals();
     geometry.computeVertexNormals();
     geometry.computeMorphNormals();
     geometry.computeBoundingSphere();
-    
+    //geometry.mergeVertices();
+    console.log("finished model!");
     return geometry;
 }
--- webgl_view/serve.py
+++ webgl_view/serve.py
@@ -49,7 +49,7 @@ class BinarySurface(tornado.web.RequestHandler):
     def post(self, subj, hemi):
         self.set_header("Content-Type", "text/plain")
         types = self.get_argument("types").split(",")
-        data = get_binary_pts(subj, types, hemi or "both")
+        data = get_binary_pts(subj, tuple(types), hemi or "both")
         self.write(data)
 
 application = tornado.web.Application([
