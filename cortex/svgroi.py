bd5386496201bea1c8ee18f3f443954e0edcd8c3
MarkLescroart <mark.lescroart@berkeley.edu>
2014-05-02 01:25:02 -0700

    Can save sulcal layers as separate ctm (so you choose whether to display sucli / rois). Need to implement multi-layer ROIpack objects to make this work the real way. Also did some improvements to saved views.
    ML

--- cortex/svgroi.py
+++ cortex/svgroi.py
@@ -374,9 +374,6 @@ class ROI(object):
 
         for path in self.paths:
             ## Deal with dashed lines
-            #print('self.dashoffset=...')
-            #print(self.dashoffset)
-            #print(" /\ dashoffset /\ ")
             if self.dashtype is None:
                 dashstr = ""
             elif self.dashtype=='fromsvg':
