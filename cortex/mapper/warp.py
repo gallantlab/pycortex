0b961c385c8ff1b2cdfbecb15064befc0c10df74
Arjun Mukerji <stormandstress@gmail.com>
2015-09-29 20:21:22 -0700

    WarpMapper class to load up warp field and sample it, currently using nearest-neighbor

--- cortex/mapper/warp.py
+++ cortex/mapper/warp.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy import sparse
 
-from . import Mapper
+from . import Mapper, _savecache
 from . import samplers
 
 class WarpMapper(Mapper):
@@ -17,11 +17,19 @@ class WarpMapper(Mapper):
 
     @classmethod
     def _cache(cls, filename, subject, xfmname, **kwargs):
-        print('Caching mapper...')
         from ..database import db
+        from ..xfm import Transform
+
+        print('Caching warpmapper...')
+        print filename, subject, xfmname
+
         masks = []
         # the next line is where the big changes will have to start...
-        xfm = db.get_xfm(subject, xfmname, xfmtype='coord')
+        # xfm = db.get_xfm(subject, xfmname, xfmtype='coord')
+        re_anat = db.get_anat(subject,'reorient_anat')
+        xfm = Transform(np.linalg.inv(re_anat.get_affine()),re_anat)
+        warp = db.get_anat(subject,'mni2anat_field')
+        warpfield = warp.get_data()
         fid = db.get_surf(subject, 'fiducial', merge=False, nudge=False)
         flat = db.get_surf(subject, 'flat', merge=False, nudge=False)
 
