1e3c52f2ab26719d98360c135a510df598d04ddb
Arjun Mukerji <stormandstress@gmail.com>
2015-11-17 11:47:27 -0800

    added the standardfile and projection (interpolation method) arguments to mni_nl, wrote some logic to use them, also correctly used the outfile so that get_surfinfo can specify the filename

--- cortex/surfinfo.py
+++ cortex/surfinfo.py
@@ -154,7 +154,7 @@ def flat_border(outfile, subject):
     
     np.savez(outfile, lines=lines, ismwalls=ismwalls)
 
-def mni_nl(subject, do=True):
+def mni_nl(outfile, subject, do=True, standardfile='', projection="lanczos"):
     """Create an automatic alignment of an anatomical image to the MNI standard.
 
     This function does the following:
@@ -181,125 +181,149 @@ def mni_nl(subject, do=True):
     cache = tempfile.mkdtemp()
 
     print('anat_to_mni, subject: %s' % subject)
-    
-    raw_anat = db.get_anat(subject, type='raw').get_filename()
-    bet_anat = db.get_anat(subject, type='brainmask').get_filename()
-    betmask_anat = db.get_anat(subject, type='brainmask_mask').get_filename()
-    anat_dir = os.path.dirname(raw_anat)
-    odir = cache
+    resp = raw_input("This function can take up to two hours to run. Is that okay? [y/n]")
+    if resp == "y":
+        raw_anat = db.get_anat(subject, type='raw').get_filename()
+        bet_anat = db.get_anat(subject, type='brainmask').get_filename()
+        betmask_anat = db.get_anat(subject, type='brainmask_mask').get_filename()
+        anat_dir = os.path.dirname(raw_anat)
+        odir = cache
+        ext = '.nii.gz'
 
-    # stem for the reoriented-into-MNI anatomical images (required by FLIRT/FNIRT)
-    reorient_anat = 'reorient_anat'
-    reorient_cmd = '{fslpre}fslreorient2std {raw_anat} {adir}/{ra_raw}'.format(fslpre=fsl_prefix,raw_anat=raw_anat, adir=odir, ra_raw=reorient_anat)
-    print('Reorienting anatomicals using fslreorient2std, cmd like: \n%s' % reorient_cmd)
-    if do and sp.call(reorient_cmd, shell=True) != 0:
-        raise IOError('Error calling fslreorient2std on raw anatomical')
-    
-    reorient_cmd = '{fslpre}fslreorient2std {bet_anat} {adir}/{ra_raw}_brain'.format(fslpre=fsl_prefix,bet_anat=bet_anat, adir=odir, ra_raw=reorient_anat)
-    if do and sp.call(reorient_cmd, shell=True) != 0:
-        raise IOError('Error calling fslreorient2std on brain-extracted anatomical')
+        if standardfile == '': #user did not specify the standard file, so we will try to determine...
+            fsl_dir = config.get("basic", "fsl_dir")
+            if not os.path.isdir(fsl_dir):
+                if 'FSLDIR' in os.environ.keys() and os.path.isdir(os.environ['FSLDIR']):
+                    fsl_dir = os.environ['FSLDIR']
+                else: # no (valid) FSLDIR env var and the config var is missing
+                    print('Could not find FSL directory. Either specify the standard you wish to use, set the fsl_dir config variable, or set $FSLDIR...')
+                    return
+            # by the time we get here, we know fsl_dir is a directory.
+            standard = os.path.join(fsl_dir,'data','standard', 'MNI152_T1_1mm')
+        else:
+            standard = standardfile
 
-    ra_betmask = reorient_anat + "_brainmask"
-    reorient_cmd = '{fslpre}fslreorient2std {bet_anat} {adir}/{ra_betmask}'.format(fslpre=fsl_prefix,bet_anat=betmask_anat, adir=odir, ra_betmask=ra_betmask)
-    
-    if do and sp.call(reorient_cmd, shell=True) != 0:
-        raise IOError('Error calling fslreorient2std on brain-extracted mask')
-            
-    fsldir = os.environ['FSLDIR']
-    standard = '%s/data/standard/MNI152_T1_1mm'%fsldir
-    bet_standard = '%s_brain'%standard
-    standardmask = '%s_mask_dil'%bet_standard
-    cout = 'mni2anat' #stem of the filenames of the transform estimates
+        if not os.path.isfile(standard) and not os.path.isfile(standard+ext): # we don't know if they passed in foo.nii.gz or just foo
+            print('The standard {sfile} does not exist! Aborting...'.format(sfile=standard))
+            return
 
-    # initial affine anatomical-to-standard registration using FLIRT. required, as the output xfm is used as a start by FNIRT.
-    flirt_cmd = '{fslpre}flirt -in {bet_standard} -ref {adir}/{ra_raw}_brain -dof 6 -omat {adir}/{cout}_flirt'
-    flirt_cmd = flirt_cmd.format(fslpre=fsl_prefix, ra_raw=reorient_anat, bet_standard=bet_standard, adir=odir, cout=cout)
-    print('Running FLIRT to estimate initial affine transform with command:\n%s'%flirt_cmd)
-    if do and sp.call(flirt_cmd, shell=True) != 0:
-        raise IOError('Error calling FLIRT with command: %s' % flirt_cmd)
+        bet_standard = '%s_brain'%standard
+        standardmask = '%s_mask_dil'%bet_standard
+        cout = 'mni2anat' #stem of the filenames of the transform estimates
+        full_cout = os.path.join(odir,cout)
 
-    # FNIRT mni-to-anat transform estimation cmd (does not apply any transform, but generates estimate [cout])
-    # the MNI152 2mm config is used even though we're referencing 1mm, per this FSL list post:
-    # https://www.jiscmail.ac.uk/cgi-bin/webadmin?A2=FSL;d14e5a9d.1105
-    cmd = '{fslpre}fnirt --in={standard} --ref={ad}/{ra_raw} --refmask={ad}/{refmask} --aff={ad}/{cout}_flirt --cout={ad}/{cout}_fnirt --fout={ad}/{cout}_field --iout={ad}/{cout}_iout --config=T1_2_MNI152_2mm'
-    cmd = cmd.format(fslpre=fsl_prefix, ra_raw=reorient_anat, standard=standard, refmask=ra_betmask, ad=odir, anat_dir=anat_dir, cout=cout)
-    print('Running FNIRT to estimate transform, using the following command... this can take a while:\n%s'%cmd)
-    if do and sp.call(cmd, shell=True) != 0:
-        raise IOError('Error calling fnirt with cmd: %s'%cmd)
+        # stem for the reoriented-into-MNI anatomical images (required by FLIRT/FNIRT)
+        reorient_anat = 'reorient_anat'
+        outfile = os.path.join(odir, reorient_anat)
+        outfile_ext = outfile + ext
+        ra_betmask = outfile + "_brainmask"
+
+        # START DOING THINGS
+        reorient_cmd = '{fslpre}fslreorient2std {raw_anat} {outfile}'.format(fslpre=fsl_prefix,raw_anat=raw_anat, outfile=outfile)
+        print('Reorienting anatomicals using fslreorient2std, cmd like: \n%s' % reorient_cmd)
+        if do and sp.call(reorient_cmd, shell=True) != 0:
+            raise IOError('Error calling fslreorient2std on raw anatomical')
+        
+        reorient_cmd = '{fslpre}fslreorient2std {bet_anat} {outfile}_brain'.format(fslpre=fsl_prefix,bet_anat=bet_anat, outfile=outfile)
+        if do and sp.call(reorient_cmd, shell=True) != 0:
+            raise IOError('Error calling fslreorient2std on brain-extracted anatomical')
+
+        reorient_cmd = '{fslpre}fslreorient2std {bet_anat} {ra_betmask}'.format(fslpre=fsl_prefix,bet_anat=betmask_anat, ra_betmask=ra_betmask)
+        if do and sp.call(reorient_cmd, shell=True) != 0:
+            raise IOError('Error calling fslreorient2std on brain-extracted mask')
+
+        # initial affine anatomical-to-standard registration using FLIRT. required, as the output xfm is used as a start by FNIRT.
+        flirt_cmd = '{fslpre}flirt -in {bet_standard} -ref {outfile}_brain -dof 6 -omat {full_cout}_flirt'
+        flirt_cmd = flirt_cmd.format(fslpre=fsl_prefix, bet_standard=bet_standard, outfile=outfile, full_cout=full_cout)
+        print('Running FLIRT to estimate initial affine transform with command:\n%s'%flirt_cmd)
+        if do and sp.call(flirt_cmd, shell=True) != 0:
+            raise IOError('Error calling FLIRT with command: %s' % flirt_cmd)
+
+        # FNIRT mni-to-anat transform estimation cmd (does not apply any transform, but generates estimate [cout])
+        # the MNI152 2mm config is used even though we're referencing 1mm, per this FSL list post:
+        # https://www.jiscmail.ac.uk/cgi-bin/webadmin?A2=FSL;d14e5a9d.1105
+        cmd = '{fslpre}fnirt --in={standard} --ref={outfile} --refmask={ra_betmask} --aff={full_cout}_flirt --cout={full_cout}_fnirt --fout={full_cout}_field --iout={full_cout}_iout --config=T1_2_MNI152_2mm'
+        cmd = cmd.format(fslpre=fsl_prefix, outfile=outfile, standard=standard, ra_betmask=ra_betmask, full_cout=full_cout)
+        print('Running FNIRT to estimate transform, using the following command... this can take a while:\n%s'%cmd)
+        if do and sp.call(cmd, shell=True) != 0:
+            raise IOError('Error calling fnirt with cmd: %s'%cmd)
 
         pts, polys = db.get_surf(subject,"fiducial",merge="True")
 
-    #print('raw anatomical: %s\nbet anatomical: %s\nflirt cmd:%s\nfnirt cmd: %s\npts: %s' % (raw_anat,bet_anat,flirt_cmd,cmd,pts))
+        #print('raw anatomical: %s\nbet anatomical: %s\nflirt cmd:%s\nfnirt cmd: %s\npts: %s' % (raw_anat,bet_anat,flirt_cmd,cmd,pts))
 
-    # take the reoriented anatomical, get its affine coord transform, invert this, and save it
-    reo_xfmnm = 'reorient_inv'
-    # need to change this line, as the reoriented anatomical is not in the db but in /tmp now
-    # re_anat = db.get_anat(subject,reorient_anat)
-    reo_anat_fn = '{odir}/{reorient_anat}.nii.gz'.format(odir=odir,reorient_anat=reorient_anat)
-    # print(reo_anat_fn)
-    # since the reoriented anatomicals aren't stored in the db anymore, db.get_anat() will not work (?)
-    re_anat = nib.load(reo_anat_fn)
-    reo_xfm = Transform(np.linalg.inv(re_anat.get_affine()),re_anat)
-    reo_xfm.save(subject,reo_xfmnm,"coord")
+        # take the reoriented anatomical, get its affine coord transform, invert this, and save it
+        reo_xfmnm = 'reorient_inv'
+        # need to change this line, as the reoriented anatomical is not in the db but in /tmp now
+        # re_anat = db.get_anat(subject,reorient_anat)
+        # since the reoriented anatomicals aren't stored in the db anymore, db.get_anat() will not work (?)
+        re_anat = nib.load(outfile_ext) # remember, we set outfile to be the full path to the reoriented anat (and outfile_ext to that+.nii.gz)
+        reo_xfm = Transform(np.linalg.inv(re_anat.get_affine()),re_anat)
+        reo_xfm.save(subject,reo_xfmnm,"coord")
 
-    # get the reoriented anatomical's qform and its inverse, they will be needed later
-    aqf = re_anat.get_qform()
-    aqfinv = np.linalg.inv(aqf)
+        # get the reoriented anatomical's qform and its inverse, they will be needed later
+        aqf = re_anat.get_qform()
+        aqfinv = np.linalg.inv(aqf)
 
-    # load the warp field data as a volume
-    # since it's not in the db anymore but in /tmp instead of:
-    # warp = db.get_anat(subject,'%s_field'%cout)
-    # it's this:
-    warp_fn = '{ad}/{cout}_field.nii.gz'.format(ad=odir,cout=cout)
-    # print warp_fn
-    warp = nib.load(warp_fn)
-    wd = warp.get_data()
-    # need in (t,z,y,x) order
-    wd = np.swapaxes(wd,0,3) # x <--> t
-    wd = np.swapaxes(wd,1,2) # y <--> z
-    wv = Volume(wd,subject,reo_xfmnm)
+        # load the warp field data as a volume
+        # since it's not in the db anymore but in /tmp instead of:
+        # warp = db.get_anat(subject,'%s_field'%cout)
+        # it's this:
+        warp_fn = '{full_cout}_field.nii.gz'.format(full_cout=full_cout)
+        # print warp_fn
+        warp = nib.load(warp_fn)
+        wd = warp.get_data()
+        # need in (t,z,y,x) order
+        wd = wd.T
+        wv = Volume(wd,subject,reo_xfmnm)
+        # need to delete the reo_xfm
+        reoxfmpath = os.path.split(reo_xfm.reference.get_filename())[0]
+        print reoxfmpath
 
-    # now do the mapping! this gets the warp field values at the corresponding points
-    # (uses fiducial surface by default)
-    warpvd = wv.map(projection="lanczos")
+        # now do the mapping! this gets the warp field values at the corresponding points
+        # (uses fiducial surface by default)
+        warpvd = wv.map(projection) # passed in via kwargs, defaults to lanczos
 
-    # reshape into something sensible
-    warpverts_L = [vs for vs in np.swapaxes(warpvd.left,0,1)]
-    warpverts_R = [vs for vs in np.swapaxes(warpvd.right,0,1)]
-    warpverts_ordered = np.concatenate((warpverts_L, warpverts_R))
+        # reshape into something sensible
+        warpverts_L = [vs for vs in np.swapaxes(warpvd.left,0,1)]
+        warpverts_R = [vs for vs in np.swapaxes(warpvd.right,0,1)]
+        warpverts_ordered = np.concatenate((warpverts_L, warpverts_R))
 
-    # append 1s for matrix multiplication (coordinate transformation)
-    o = np.ones((len(pts),1))
-    pad_pts = np.append(pts, o, axis=1)
+        # append 1s for matrix multiplication (coordinate transformation)
+        o = np.ones((len(pts),1))
+        pad_pts = np.append(pts, o, axis=1)
 
-    # print pts, len(pts), len(pts[0]), warpverts_ordered, len(warpverts_ordered), pad_pts, len(pad_pts), pad_pts[0]
+        # print pts, len(pts), len(pts[0]), warpverts_ordered, len(warpverts_ordered), pad_pts, len(pad_pts), pad_pts[0]
 
-    # transform vertex coords from mm to vox using the anat's qform
-    voxcoords = [aqfinv.dot(padpt) for padpt in pad_pts]
-    # add the offsets specified in the warp at those locations (ignoring the 1s here)
-    mnivoxcoords = [voxcoords[n][:-1] + warpverts_ordered[n] for n in range(len(voxcoords))]
-    # re-pad for matrix multiplication
-    pad_mnivox = np.append(mnivoxcoords, o, axis=1)
+        # transform vertex coords from mm to vox using the anat's qform
+        voxcoords = [aqfinv.dot(padpt) for padpt in pad_pts]
+        # add the offsets specified in the warp at those locations (ignoring the 1s here)
+        mnivoxcoords = [voxcoords[n][:-1] + warpverts_ordered[n] for n in range(len(voxcoords))]
+        # re-pad for matrix multiplication
+        pad_mnivox = np.append(mnivoxcoords, o, axis=1)
 
-    # multiply by the standard's qform to recover mm coords
-    std = nib.load('%s.nii.gz'%standard)
-    stdqf = std.get_qform()
-    mni_coords = np.array([stdqf.dot(padmni)[:-1] for padmni in pad_mnivox])
+        # multiply by the standard's qform to recover mm coords
 
-    # some debug output
-    # print pts, mni_coords
-    # print pts[0], mni_coords[0]
-    # print len(pts), len(mni_coords)
-    # print type(pts), type(pts[0][0]), type(mni_coords)
+        if not os.path.splitext(standard)[1]: #will be True iff no extension specified in standard file name
+            std = nib.load('%s.nii.gz'%standard)
+        else:
+            std = nib.load(standard)
+        stdqf = std.get_qform()
+        mni_coords = np.array([stdqf.dot(padmni)[:-1] for padmni in pad_mnivox])
 
-    # now split mni_coords into left and right arrays for saving
-    nverts_L = len(warpverts_L)
-    #print nverts_L
-    left = mni_coords[:nverts_L]
-    right = mni_coords[nverts_L:]
-    #print len(left), len(right)
+        # some debug output
+        # print pts, mni_coords
+        # print pts[0], mni_coords[0]
+        # print len(pts), len(mni_coords)
+        # print type(pts), type(pts[0][0]), type(mni_coords)
 
-    mni_surfinfo_fn = db.get_paths(subject)['surfinfo'].format(type='mnicoords',opts='')
-    print('Saving mni coordinates as a surfinfo...')
-    np.savez(mni_surfinfo_fn,leftpts=left,rightpts=right)
+        # now split mni_coords into left and right arrays for saving
+        nverts_L = len(warpverts_L)
+        #print nverts_L
+        left = mni_coords[:nverts_L]
+        right = mni_coords[nverts_L:]
+        #print len(left), len(right)
+
+        print('Saving MNI coordinates as a surfinfo ({outfile})...'.format(outfile=outfile))
+        np.savez(outfile,leftpts=left,rightpts=right)
 
