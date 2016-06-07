8aede57c02d33b0cec66ecae02086bff467f8cb5
James Gao <james@jamesgao.com>
2016-06-07 13:49:51 -0700

    wrong import

--- cortex/polyutils.py
+++ cortex/polyutils.py
@@ -986,7 +986,7 @@ def measure_volume(pts, polys):
     return mp.volume
 
 def marching_cubes(volume, smooth=True, decimate=True, **kwargs):
-    import tvtk
+    from tvtk.api import tvtk
     imgdata = tvtk.ImageData(dimensions=volume.shape)
     imgdata.point_data.scalars = volume.flatten('F')
 
