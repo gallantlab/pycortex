e32b191faf42cae752e9174a164e4d38de99fae9
Mark Lescroart <mark.lescroart@gmail.com>
2016-10-31 11:54:13 -0700

    removal of extraneous print calls

--- cortex/webgl/view.py
+++ cortex/webgl/view.py
@@ -225,7 +225,6 @@ def show(data, types=("inflated",), recache=False, cmap='RdBu_r', layout=None,
         which server port is running the web page) This argument replaces `open_browser`,
         which still works (2016.08.17) but is deprecated.
     """
-    print('Testing new webshow...')
     data = dataset.normalize(data)
     if not isinstance(data, dataset.Dataset):
         data = dataset.Dataset(data=data)
@@ -708,7 +707,6 @@ def show(data, types=("inflated",), recache=False, cmap='RdBu_r', layout=None,
         url_stub = 'https://data8.berkeley.edu/user/%s/proxy/%d'%(user, port)
         if url is None:
             url = url_stub+'/%s'%(template)
-            print(url)
     else:
         url_stub = ''
         url = "http://%s:%d/%s"%(serve.hostname, port, template)
