6d6f7614ebecc3558208e90cf0ac2b1f9f78ea62
alexhuth <alex.huth@gmail.com>
2017-03-07 15:59:27 -0800

    DOC: changed so only examples starting with plot_ are run

--- docs/conf.py
+++ docs/conf.py
@@ -43,8 +43,8 @@ sphinx_gallery_conf = {
     'examples_dirs' : '../examples',
     # path where to save gallery generated examples
     'gallery_dirs'  : 'auto_examples',
-    # which files to execute? all of them
-    'filename_pattern' : '/*py'}
+    # which files to execute? only those starting with "plot_"
+    'filename_pattern' : '/plot_*py'}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
