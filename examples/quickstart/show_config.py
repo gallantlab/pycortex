1e0e8e3066470dfae26d3b6ca67cf7271279a5e1
Tianjiao Zhang <t.zhang@berkeley.edu>
2017-03-09 16:31:09 -0800

    doc: show_config.py other options

--- examples/quickstart/show_config.py
+++ examples/quickstart/show_config.py
@@ -15,7 +15,22 @@ print(cortex.options.usercfg)
 
 ##########################################################
 # Finding where the current filestore is.
-# Useful for when your subjects don't show up in cortex.db
+# Useful for when your subjects don't show up in cortex.db, and all you have is S1
 print(cortex.options.get('basic', 'filestore'))
 
-############################################
+##########################################################
+# Finding where pycortex is looking for colormaps
+# Useful for when you get color map not found messages
+print(cortex.options.get('webgl', 'colormaps'))
+
+##########################################################
+# To look at your config file, it is recommended that you open it with a text editor.
+# However, you *can* still look at options from within pycortex
+
+# sections gets the upper-level sections in the config file
+sections = cortex.options.sections()
+print(sections)
+
+# items gets the option items within a section as a list of key-value pairs.
+basic_config = cortex.options.items('roi')
+print(basic_config)
