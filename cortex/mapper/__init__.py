0bd3b42bc8f142190749d22615946114bee4271d
alexhuth <alex.huth@gmail.com>
2017-04-14 14:26:00 -0700

    Added proper vertex-to-voxel mapper, moved mapper class to its own file

--- cortex/mapper/__init__.py
+++ cortex/mapper/__init__.py
@@ -1,12 +1,9 @@
 import os
-import warnings
 
-import nibabel
 import numpy as np
-from scipy import sparse
-warnings.simplefilter('ignore', sparse.SparseEfficiencyWarning)
 
 from .. import dataset
+from mapper import Mapper
 
 def get_mapper(subject, xfmname, type='nearest', recache=False, **kwargs):
     from ..database import db
@@ -51,125 +48,3 @@ def _savecache(filename, left, right, shape):
             right_indptr=right.indptr,
             right_shape=right.shape,
             shape=shape)
-
-class Mapper(object):
-    '''Maps data from epi volume onto surface using various projections'''
-    def __init__(self, left, right, shape):
-        self.idxmap = None
-        self.masks = [left, right]
-        self.nverts = left.shape[0] + right.shape[0]
-        self.shape = shape
-
-    @classmethod
-    def from_cache(cls, cachefile):
-        npz = np.load(cachefile)
-        left = (npz['left_data'], npz['left_indices'], npz['left_indptr'])
-        right = (npz['right_data'], npz['right_indices'], npz['right_indptr'])
-        lsparse = sparse.csr_matrix(left, shape=npz['left_shape'])
-        rsparse = sparse.csr_matrix(right, shape=npz['right_shape'])
-        return cls(lsparse, rsparse, npz['shape'])
-
-    @property
-    def mask(self):
-        mask = np.array(self.masks[0].sum(0) + self.masks[1].sum(0))
-        return (mask.squeeze() != 0).reshape(self.shape)
-
-    @property
-    def hemimasks(self):
-        func = lambda m: (np.array(m.sum(0)).squeeze() != 0).reshape(self.shape)
-        #return list(map(func, self.masks))
-        return [func(x) for x in self.masks]
-
-    def __repr__(self):
-        ptype = self.__class__.__name__
-        return '<%s mapper with %d vertices>'%(ptype, self.nverts)
-
-    def __call__(self, data):
-        if isinstance(data, tuple):
-            data = dataset.Volume(*data)
-
-        if isinstance(data, dataset.Vertex):
-            llen = self.masks[0].shape[0]
-            if data.raw:
-                left, right = data.data[..., :llen,:], data.data[..., llen:,:]
-                if self.idxmap is not None:
-                    left = left[..., self.idxmap[0], :]
-                    right = right[..., self.idxmap[1], :]
-            else:
-                left, right = data[..., :llen], data[..., llen:]
-                if self.idxmap is not None:
-                    left = left[..., self.idxmap[0]]
-                    right = right[..., self.idxmap[1]]
-            return left, right
-
-        volume = np.ascontiguousarray(data.volume)
-        volume.shape = len(volume), -1
-        volume = volume.T
-
-        mapped = []
-        for mask in self.masks:
-            mapped.append(np.array(mask * volume).T)
-
-        if self.idxmap is not None:
-            mapped[0] = mapped[0][:, self.idxmap[0]]
-            mapped[1] = mapped[1][:, self.idxmap[1]]
-
-        return dataset.Vertex(np.hstack(mapped).squeeze(), data.subject)
-
-    def backwards(self, verts, fast=True):
-        '''Projects vertex data back into volume space
-
-        Parameters
-        ----------
-        verts : array_like
-            If uint array and max <= nverts, assume binary mask of vertices
-            If float array and len == nverts, project float values into volume
-        '''
-        left = np.zeros((self.masks[0].shape[0],), dtype=bool)
-        right = np.zeros((self.masks[1].shape[0],), dtype=bool)
-
-        if len(verts) > 0:
-            if isinstance(verts, (list, tuple)) and len(verts) == 2:
-                if len(verts[0]) == len(left):
-                    left = verts[0]
-                    right = verts[1]
-                elif verts[0].max() < len(left):
-                    left[verts[0]] = True
-                    right[verts[1]] = True
-                else:
-                    raise ValueError
-            else:
-                if len(verts) == self.nverts:
-                    left = verts[:len(left)]
-                    right = verts[len(left):]
-                elif verts.max() < self.nverts:
-                    left[verts[verts < len(left)]] = True
-                    right[verts[verts >= len(left)] - len(left)] = True
-                else:
-                    raise ValueError
-
-        output = []
-        for mask, data in zip(self.masks, [left, right]):
-            proj = data * mask
-            output.append(np.array(proj).reshape(self.shape))
-
-        return output
-
-    @classmethod
-    def _cache(cls, filename, subject, xfmname, **kwargs):
-        print('Caching mapper...')
-        from ..database import db
-        masks = []
-        xfm = db.get_xfm(subject, xfmname, xfmtype='coord')
-        fid = db.get_surf(subject, 'fiducial', merge=False, nudge=False)
-
-        try:
-            flat = db.get_surf(subject, 'flat', merge=False, nudge=False)
-        except IOError:
-            flat = fid
-
-        for (pts, _), (_, polys) in zip(fid, flat):
-            masks.append(cls._getmask(xfm(pts), polys, xfm.shape, **kwargs))
-
-        _savecache(filename, masks[0], masks[1], xfm.shape)
-        return cls(masks[0], masks[1], xfm.shape)
---
+++ cortex/mapper/mapper.py
@@ -0,0 +1,122 @@
+import numpy as np
+from scipy import sparse
+
+from .. import dataset
+
+import warnings
+warnings.simplefilter('ignore', sparse.SparseEfficiencyWarning)
+
+class Mapper(object):
+    '''Maps data from epi volume onto surface using various projections'''
+    def __init__(self, left, right, shape, subject, xfmname):
+        self.idxmap = None
+        self.masks = [left, right]
+        self.nverts = left.shape[0] + right.shape[0]
+        self.shape = shape
+        self.subject = subject
+        self.xfmname = xfmname
+
+    @classmethod
+    def from_cache(cls, cachefile):
+        npz = np.load(cachefile)
+        left = (npz['left_data'], npz['left_indices'], npz['left_indptr'])
+        right = (npz['right_data'], npz['right_indices'], npz['right_indptr'])
+        lsparse = sparse.csr_matrix(left, shape=npz['left_shape'])
+        rsparse = sparse.csr_matrix(right, shape=npz['right_shape'])
+        return cls(lsparse, rsparse, npz['shape'])
+
+    @property
+    def mask(self):
+        mask = np.array(self.masks[0].sum(0) + self.masks[1].sum(0))
+        return (mask.squeeze() != 0).reshape(self.shape)
+
+    @property
+    def hemimasks(self):
+        func = lambda m: (np.array(m.sum(0)).squeeze() != 0).reshape(self.shape)
+        #return list(map(func, self.masks))
+        return [func(x) for x in self.masks]
+
+    def __repr__(self):
+        ptype = self.__class__.__name__
+        return '<%s mapper with %d vertices>'%(ptype, self.nverts)
+
+    def __call__(self, data):
+        if isinstance(data, tuple):
+            data = dataset.Volume(*data)
+
+        if isinstance(data, dataset.Vertex):
+            llen = self.masks[0].shape[0]
+            if data.raw:
+                left, right = data.data[..., :llen,:], data.data[..., llen:,:]
+                if self.idxmap is not None:
+                    left = left[..., self.idxmap[0], :]
+                    right = right[..., self.idxmap[1], :]
+            else:
+                left, right = data[..., :llen], data[..., llen:]
+                if self.idxmap is not None:
+                    left = left[..., self.idxmap[0]]
+                    right = right[..., self.idxmap[1]]
+            return left, right
+
+        volume = np.ascontiguousarray(data.volume)
+        volume.shape = len(volume), -1
+        volume = volume.T
+
+        mapped = []
+        for mask in self.masks:
+            mapped.append(np.array(mask * volume).T)
+
+        if self.idxmap is not None:
+            mapped[0] = mapped[0][:, self.idxmap[0]]
+            mapped[1] = mapped[1][:, self.idxmap[1]]
+
+        return dataset.Vertex(np.hstack(mapped).squeeze(), data.subject)
+
+    def backwards(self, vertexdata):
+        '''Projects vertex data back into volume space.
+
+        Parameters
+        ----------
+        vertexdata : Vertex object
+            The data that will be projected back into voxel space.
+        '''
+        # stack the two mappers together
+        bothmappers = sparse.vstack(self.masks)
+        # dot the vertex data with the stacked mappers
+        partial_vertex = bothmappers.T.dot(vertexdata.data)
+        # solve the inverse mapping problem
+        voxeldata = self._get_backmapper().solve(partial_vertex).reshape(self.shape)
+        # construct a volume object with the new data
+        return dataset.Volume(voxeldata, self.subject, self.xfmname)
+
+    def _get_backmapper(self):
+        if not hasattr(self, '_backmapper'):
+            # stack the two mappers together to get one voxel -> vertex mapper
+            bothmappers = sparse.vstack(self.masks)
+            # take inner product to get symmetric matrix
+            symmappers = bothmappers.T.dot(bothmappers)
+            # add (very) small diagonal to make sure it's full rank
+            symmappers_reg = symmappers + 1e-9 * sparse.eye(symmappers.shape[0])
+            # factorize it using splu so that inversion is fast
+            self._backmapper = sparse.linalg.splu(symmappers_reg)
+
+        return self._backmapper
+
+    @classmethod
+    def _cache(cls, filename, subject, xfmname, **kwargs):
+        print('Caching mapper...')
+        from ..database import db
+        masks = []
+        xfm = db.get_xfm(subject, xfmname, xfmtype='coord')
+        fid = db.get_surf(subject, 'fiducial', merge=False, nudge=False)
+
+        try:
+            flat = db.get_surf(subject, 'flat', merge=False, nudge=False)
+        except IOError:
+            flat = fid
+
+        for (pts, _), (_, polys) in zip(fid, flat):
+            masks.append(cls._getmask(xfm(pts), polys, xfm.shape, **kwargs))
+
+        _savecache(filename, masks[0], masks[1], xfm.shape)
+        return cls(masks[0], masks[1], xfm.shape, subject, xfmname)
