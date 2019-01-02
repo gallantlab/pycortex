2bb26d35ec36c27fa777c8bea77eeddc4576c933
Anwar Nunez-Elizalde <anwarnunez@users.noreply.github.com>
2019-01-02 15:41:27 -0800

    DOC: example write-up

--- examples/import_surface/import_fmriprep.py
+++ examples/import_surface/import_fmriprep.py
@@ -11,14 +11,22 @@ This example is based on the fmriprep 1.0.15 output of openfmri ds000164 that ca
 
 import cortex
 from cortex import fmriprep
+from os import path as op
 
-fmriprep.import_subj('001', '/derivatives/ds000164')
+# Location of the downloaded openfmri dataset
+source_directory = '/derivatives/ds000164'
+# fmriprep subject name (without "sub-")
+subject_id = '001' 
 
-# We can use the identity transform to visualize the T1-weighted image
-t1w = '/derivatives/ds000164/fmriprep/sub-001/anat/sub-001_T1w_preproc.nii.gz'
+# import subject into pycortex database
+fmriprep.import_subj(subject_id, source_directory)
 
-# Now we can make a volume
-t1w_volume = cortex.Volume(t1w, '001', 'identity')
+# We can use visualize the imported subject's T1-weighted image
+anat_nifti = 'fmriprep/sub-001/anat/sub-001_T1w_preproc.nii.gz'
+t1_image_path = op.join(source_directory, anat_nifti)
+
+# Now we can make a volume using the built-in identity transform
+t1w_volume = cortex.Volume(t1_image_path, subject_id, 'identity')
 
 # And show the result.
 ds = cortex.Dataset(t1w=t1w_volume)
