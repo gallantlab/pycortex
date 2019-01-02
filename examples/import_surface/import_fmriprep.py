4fa32c097dbb5e24d285f98b0f022c022c6e81cf
Anwar Nunez-Elizalde <anwarnunez@users.noreply.github.com>
2019-01-02 15:15:56 -0800

    minor: use built-in `identity` transform

--- examples/import_surface/import_fmriprep.py
+++ examples/import_surface/import_fmriprep.py
@@ -17,15 +17,10 @@ import cortex
 cortex.fmriprep.import_subj('001', '/derivatives/ds000164')
 
 # We can use the identity transform to visualize the T1-weighted image
-import numpy as np
-
-ident = np.identity(4)
 t1w = '/derivatives/ds000164/fmriprep/sub-001/anat/sub-001_T1w_preproc.nii.gz'
-transform = cortex.xfm.Transform(ident, t1w)
-transform.save('001', 'ident.t1w', 'magnet')
 
 # Now we can make a volume
-t1w_volume = cortex.Volume(t1w, '001', 'ident.t1w')
+t1w_volume = cortex.Volume(t1w, '001', 'identity')
 
 # And show the result.
 ds = cortex.Dataset(t1w=t1w_volume)
