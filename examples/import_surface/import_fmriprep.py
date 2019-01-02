330ee4d72f92f238994dd77f34712c0f6f291f3e
Anwar Nunez-Elizalde <anwarnunez@users.noreply.github.com>
2019-01-02 15:26:55 -0800

    DOC: single line

--- examples/import_surface/import_fmriprep.py
+++ examples/import_surface/import_fmriprep.py
@@ -3,13 +3,10 @@
 Import fmriprep output
 ==================
 
-Recently, many people have start to use fmriprep as a complete preprocessing
-workflow of anatomical and functional data. Pycortex has a convenience function to import
+Recently, many people have start to use fmriprep as a complete preprocessing workflow of anatomical and functional data. Pycortex has a convenience function to import
 the output of this workflow.
 
-This example is based on the fmriprep 1.0.15 output of openfmri ds000164 that can be found
-on openneuro.org:
-https://openneuro.org/datasets/ds000164/versions/00001
+This example is based on the fmriprep 1.0.15 output of openfmri ds000164 that can be found on openneuro.org: https://openneuro.org/datasets/ds000164/versions/00001
 """
 
 import cortex
