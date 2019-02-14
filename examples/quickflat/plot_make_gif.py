e0dd9151f7a23329e9cef01a160cd76ab9dccf3f
Robert Gibboni <galileo@gmail.com>
2019-02-14 13:46:33 -0800

    Fixes to GIF example

--- examples/quickflat/plot_make_gif.py
+++ examples/quickflat/plot_make_gif.py
@@ -1,22 +1,11 @@
 """
-===============================
-Save a 2D static flatmap as PNG
-===============================
+====================================
+Animate a series of volumes as a GIF
+====================================
 
-Plot a 2D static flatmap and save it as PNG file.
-
-**Some words on the `recache` parameter before we begin:**
-
-Setting the `recache=True` parameter recaches the flatmap cache located in
-<filestore>/<subject>/cache. By default intermediate steps for a flatmap are
-cached after the first generation to speed up the process for the future. If
-any of the intermediate steps changes, the flatmap generation may fail.
-`recache=True` will load these intermediate steps new.
-This can be helpful if you think there is no reason that the
-`quickflat.make_figure` to fail but it nevertheless fails. Try it, it's magic!
-
-The default background is set to be a transparent image. If you want to change
-that use the parameter `bgcolor`.
+A convenient way to compare two flat maps (e.g., prediction performance or
+tuning weights) is to flip back and forth between them. This example shows how
+to make an animated gif in which each frame is a flatmap.
 
 """
 import cortex
@@ -24,15 +13,15 @@ import numpy as np
 np.random.seed(1234)
 
 # Create several pycortex Volumes
-volumes = {'first': cortex.Volume.random(subject='S1', xfmname='fullhead'),
-           'second': cortex.Volume.random(subject='S1', xfmname='fullhead')}
+volumes = {'first': cortex.Volume.random(subject='S1', xfmname='fullhead', vmin=-2, vmax=2),
+           'second': cortex.Volume.random(subject='S1', xfmname='fullhead', vmin=-2, vmax=2)}
 
 # Generate an animated gif that switches between frames every 1.5 seconds
 filename = "./my_flatmap.gif"
 cortex.quickflat.make_gif(filename, volumes, frame_duration=1.5)
 
 
-# Display gif inline in an IPython note
+# Display gif inline in an IPython notebook
 import io
 from IPython.display import Image
 
