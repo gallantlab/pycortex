5bc27392e2f422880c69e491f597aabe198c9564
Robert Gibboni <galileo@gmail.com>
2019-02-13 16:51:30 -0800

    Add make_gif to init

--- cortex/quickflat/__init__.py
+++ cortex/quickflat/__init__.py
@@ -1,3 +1,3 @@
-from .view import make_figure, make_png, make_svg, make_movie
+from .view import make_figure, make_png, make_svg, make_movie, make_gif
 from .utils import make_flatmap_image
 from . import composite
