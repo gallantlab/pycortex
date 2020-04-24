0fa76dbd7c511723fca01cae512d60a91796906b
Matteo Visconti di Oleggio Castello <mvdoc@users.noreply.github.com>
2020-04-23 18:52:24 -0700

    ENH add new view with inflated and lateral, medial, ventral views (#361)
    * ENH add new view with inflated and lateral, medial, ventral views
    
    * CLN remove duplicate import [skip ci]

--- cortex/export/__init__.py
+++ cortex/export/__init__.py
@@ -2,10 +2,12 @@ from .save_views import save_3d_views
 from .panels import plot_panels
 from .panels import params_flatmap_lateral_medial
 from .panels import params_occipital_triple_view
+from .panels import params_inflated_lateral_medial_ventral
 
 __all__ = [
     'save_3d_views',
     'plot_panels',
     'params_flatmap_lateral_medial',
     'params_occipital_triple_view',
+    'params_inflated_lateral_medial_ventral'
 ]
