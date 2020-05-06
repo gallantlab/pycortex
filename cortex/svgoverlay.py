bb3f6b0034c54936c232f20a548605fc1a3ca9ce
Matteo Visconti di Oleggio Castello <mvdoc@users.noreply.github.com>
2020-05-06 12:04:25 -0700

    FIX check inkscape version and modify cmd (#365)
    * FIX check inkscape version and modify cmd
    
    * ENH use LooseVersion to check versions

--- cortex/svgoverlay.py
+++ cortex/svgoverlay.py
@@ -11,10 +11,13 @@ from matplotlib.path import Path
 from scipy.spatial import cKDTree
 from builtins import zip, str
 
+from distutils.version import LooseVersion
+
 from lxml import etree
 from lxml.builder import E
 
 from .options import config
+from .testing_utils import INKSCAPE_VERSION
 
 svgns = "http://www.w3.org/2000/svg"
 inkns = "http://www.inkscape.org/namespaces/inkscape"
@@ -265,7 +268,11 @@ class SVGOverlay(object):
             pngfile = png.name
 
         inkscape_cmd = config.get('dependency_paths', 'inkscape')
-        cmd = "{inkscape_cmd} -z -h {height} -e {outfile} /dev/stdin"
+        if LooseVersion(INKSCAPE_VERSION) < LooseVersion('1.0'):
+            cmd = "{inkscape_cmd} -z -h {height} -e {outfile} /dev/stdin"
+        else:
+            cmd = "{inkscape_cmd} -h {height} --export-filename {outfile} " \
+                  "/dev/stdin"
         cmd = cmd.format(inkscape_cmd=inkscape_cmd, height=height, outfile=pngfile)
         proc = sp.Popen(shlex.split(cmd), stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE)
         stdout, stderr = proc.communicate(etree.tostring(self.svg))
--- cortex/testing_utils.py
+++ cortex/testing_utils.py
@@ -1,7 +1,24 @@
 """Module containing utils for testing"""
+import subprocess as sp
 from shutil import which
 
 
 def has_installed(name):
     return which(name) is not None
+
+
+def inkscape_version():
+    if not has_installed('inkscape'):
+        return None
+    cmd = 'inkscape --version'
+    output = sp.check_output(cmd.split(), stderr=sp.PIPE)
+    # b'Inkscape 1.0 (4035a4f, 2020-05-01)\n'
+    version = output.split()[1]
+    if isinstance(version, bytes):
+        version = version.decode('utf-8')
+    return version
+
+
+INKSCAPE_VERSION = inkscape_version()
+
 
