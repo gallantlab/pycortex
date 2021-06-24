aedf41258d8a9737b29106c71e23f6a37556be23
Matteo Visconti di Oleggio Castello <mvdoc@berkeley.edu>
2021-06-24 14:52:43 -0700

    MNT black _default_params.py

--- cortex/export/_default_params.py
+++ cortex/export/_default_params.py
@@ -1,148 +1,159 @@
 params_flatmap_lateral_medial = {
-    'figsize': [16, 9],
-    'panels': [
+    "figsize": [16, 9],
+    "panels": [
         {
-            'extent': [0.000, 0.200, 1.000, 0.800],
-            'view': {
-                'angle': 'flatmap',
-                'surface': 'flatmap'
+            "extent": [0.000, 0.200, 1.000, 0.800],
+            "view": {"angle": "flatmap", "surface": "flatmap"},
+        },
+        {
+            "extent": [0.300, 0.000, 0.200, 0.200],
+            "view": {
+                "hemisphere": "left",
+                "angle": "medial_pivot",
+                "surface": "inflated",
             },
         },
         {
-            'extent': [0.300, 0.000, 0.200, 0.200],
-            'view': {
-                'hemisphere': 'left',
-                'angle': 'medial_pivot',
-                'surface': 'inflated'
-            }
-        },
-        {
-            'extent': [0.500, 0.000, 0.200, 0.200],
-            'view': {
-                'hemisphere': 'right',
-                'angle': 'medial_pivot',
-                'surface': 'inflated'
+            "extent": [0.500, 0.000, 0.200, 0.200],
+            "view": {
+                "hemisphere": "right",
+                "angle": "medial_pivot",
+                "surface": "inflated",
             },
         },
         {
-            'extent': [0.000, 0.000, 0.300, 0.300],
-            'view': {
-                'hemisphere': 'left',
-                'angle': 'lateral_pivot',
-                'surface': 'inflated'
-            }
-        },
-        {
-            'extent': [0.700, 0.000, 0.300, 0.300],
-            'view': {
-                'hemisphere': 'right',
-                'angle': 'lateral_pivot',
-                'surface': 'inflated'
+            "extent": [0.000, 0.000, 0.300, 0.300],
+            "view": {
+                "hemisphere": "left",
+                "angle": "lateral_pivot",
+                "surface": "inflated",
             },
         },
-    ]
+        {
+            "extent": [0.700, 0.000, 0.300, 0.300],
+            "view": {
+                "hemisphere": "right",
+                "angle": "lateral_pivot",
+                "surface": "inflated",
+            },
+        },
+    ],
 }
 
 params_occipital_triple_view = {
-    'figsize': [16, 9],
-    'panels': [{
-        'extent': [0.260, 0.000, 0.480, 1.000],
-        'view': {
-            'angle': 'flatmap',
-            'surface': 'flatmap',
-            'zoom': [0.250, 0.000, 0.500, 1.000]
-        }
-    }, {
-        'extent': [0.000, 0.000, 0.250, 0.333],
-        'view': {
-            'hemisphere': 'left',
-            'angle': 'bottom_pivot',
-            'surface': 'inflated'
-        }
-    }, {
-        'extent': [0.000, 0.333, 0.250, 0.333],
-        'view': {
-            'hemisphere': 'left',
-            'angle': 'medial_pivot',
-            'surface': 'inflated'
-        }
-    }, {
-        'extent': [0.000, 0.666, 0.250, 0.333],
-        'view': {
-            'hemisphere': 'left',
-            'angle': 'lateral_pivot',
-            'surface': 'inflated'
-        }
-    }, {
-        'extent': [0.750, 0.000, 0.250, 0.333],
-        'view': {
-            'hemisphere': 'right',
-            'angle': 'bottom_pivot',
-            'surface': 'inflated'
-        }
-    }, {
-        'extent': [0.750, 0.333, 0.250, 0.333],
-        'view': {
-            'hemisphere': 'right',
-            'angle': 'medial_pivot',
-            'surface': 'inflated'
-        }
-    }, {
-        'extent': [0.750, 0.666, 0.250, 0.333],
-        'view': {
-            'hemisphere': 'right',
-            'angle': 'lateral_pivot',
-            'surface': 'inflated'
-        }
-    }]
+    "figsize": [16, 9],
+    "panels": [
+        {
+            "extent": [0.260, 0.000, 0.480, 1.000],
+            "view": {
+                "angle": "flatmap",
+                "surface": "flatmap",
+                "zoom": [0.250, 0.000, 0.500, 1.000],
+            },
+        },
+        {
+            "extent": [0.000, 0.000, 0.250, 0.333],
+            "view": {
+                "hemisphere": "left",
+                "angle": "bottom_pivot",
+                "surface": "inflated",
+            },
+        },
+        {
+            "extent": [0.000, 0.333, 0.250, 0.333],
+            "view": {
+                "hemisphere": "left",
+                "angle": "medial_pivot",
+                "surface": "inflated",
+            },
+        },
+        {
+            "extent": [0.000, 0.666, 0.250, 0.333],
+            "view": {
+                "hemisphere": "left",
+                "angle": "lateral_pivot",
+                "surface": "inflated",
+            },
+        },
+        {
+            "extent": [0.750, 0.000, 0.250, 0.333],
+            "view": {
+                "hemisphere": "right",
+                "angle": "bottom_pivot",
+                "surface": "inflated",
+            },
+        },
+        {
+            "extent": [0.750, 0.333, 0.250, 0.333],
+            "view": {
+                "hemisphere": "right",
+                "angle": "medial_pivot",
+                "surface": "inflated",
+            },
+        },
+        {
+            "extent": [0.750, 0.666, 0.250, 0.333],
+            "view": {
+                "hemisphere": "right",
+                "angle": "lateral_pivot",
+                "surface": "inflated",
+            },
+        },
+    ],
 }
 
 params_inflated_lateral_medial_ventral = {
-    'figsize': [10, 9],
-    'panels': [
+    "figsize": [10, 9],
+    "panels": [
         {
-            'extent': [0.0, 0.0, 0.5, 1/3.],
-            'view': {
-                'hemisphere': 'left',
-                'angle': 'bottom_pivot',
-                'surface': 'inflated_less'
-            }
-        }, {
-            'extent': [0.000, 1/3., 0.5, 1/3.],
-            'view': {
-                'hemisphere': 'left',
-                'angle': 'medial_pivot',
-                'surface': 'inflated_less'
-            }
-        }, {
-            'extent': [0.000, 2/3., 0.5, 1/3.],
-            'view': {
-                'hemisphere': 'left',
-                'angle': 'lateral_pivot',
-                'surface': 'inflated_less'
-            }
-        }, {
-            'extent': [0.5, 0.0, 0.5, 1/3.],
-            'view': {
-                'hemisphere': 'right',
-                'angle': 'bottom_pivot',
-                'surface': 'inflated_less'
-            }
-        }, {
-            'extent': [0.5, 1/3., 0.5, 1/3.],
-            'view': {
-                'hemisphere': 'right',
-                'angle': 'medial_pivot',
-                'surface': 'inflated_less'
-            }
-        }, {
-            'extent': [0.5, 2/3., 0.5, 1/3.],
-            'view': {
-                'hemisphere': 'right',
-                'angle': 'lateral_pivot',
-                'surface': 'inflated_less'
-            }
-        }]
+            "extent": [0.0, 0.0, 0.5, 1 / 3.0],
+            "view": {
+                "hemisphere": "left",
+                "angle": "bottom_pivot",
+                "surface": "inflated_less",
+            },
+        },
+        {
+            "extent": [0.000, 1 / 3.0, 0.5, 1 / 3.0],
+            "view": {
+                "hemisphere": "left",
+                "angle": "medial_pivot",
+                "surface": "inflated_less",
+            },
+        },
+        {
+            "extent": [0.000, 2 / 3.0, 0.5, 1 / 3.0],
+            "view": {
+                "hemisphere": "left",
+                "angle": "lateral_pivot",
+                "surface": "inflated_less",
+            },
+        },
+        {
+            "extent": [0.5, 0.0, 0.5, 1 / 3.0],
+            "view": {
+                "hemisphere": "right",
+                "angle": "bottom_pivot",
+                "surface": "inflated_less",
+            },
+        },
+        {
+            "extent": [0.5, 1 / 3.0, 0.5, 1 / 3.0],
+            "view": {
+                "hemisphere": "right",
+                "angle": "medial_pivot",
+                "surface": "inflated_less",
+            },
+        },
+        {
+            "extent": [0.5, 2 / 3.0, 0.5, 1 / 3.0],
+            "view": {
+                "hemisphere": "right",
+                "angle": "lateral_pivot",
+                "surface": "inflated_less",
+            },
+        },
+    ],
 }
 
 
@@ -150,7 +161,7 @@ params_inflated_dorsal_lateral_medial_ventral = {
     "figsize": [10, 10],
     "panels": [
         {
-            "extent": [0.0, 0.0, 0.5, .20],
+            "extent": [0.0, 0.0, 0.5, 0.20],
             "view": {
                 "hemisphere": "left",
                 "angle": "bottom_pivot",
@@ -158,7 +169,7 @@ params_inflated_dorsal_lateral_medial_ventral = {
             },
         },
         {
-            "extent": [0.000, .20, 0.5, .3],
+            "extent": [0.000, 0.20, 0.5, 0.3],
             "view": {
                 "hemisphere": "left",
                 "angle": "medial_pivot",
@@ -166,7 +177,7 @@ params_inflated_dorsal_lateral_medial_ventral = {
             },
         },
         {
-            "extent": [0.000, .50, 0.5, .3],
+            "extent": [0.000, 0.50, 0.5, 0.3],
             "view": {
                 "hemisphere": "left",
                 "angle": "lateral_pivot",
@@ -174,7 +185,7 @@ params_inflated_dorsal_lateral_medial_ventral = {
             },
         },
         {
-            "extent": [0.000, .80, 0.5, .20],
+            "extent": [0.000, 0.80, 0.5, 0.20],
             "view": {
                 "hemisphere": "left",
                 "angle": "top_pivot",
@@ -182,7 +193,7 @@ params_inflated_dorsal_lateral_medial_ventral = {
             },
         },
         {
-            "extent": [0.5, 0.0, 0.5, .20],
+            "extent": [0.5, 0.0, 0.5, 0.20],
             "view": {
                 "hemisphere": "right",
                 "angle": "bottom_pivot",
@@ -190,7 +201,7 @@ params_inflated_dorsal_lateral_medial_ventral = {
             },
         },
         {
-            "extent": [0.5, .20, 0.5, .30],
+            "extent": [0.5, 0.20, 0.5, 0.30],
             "view": {
                 "hemisphere": "right",
                 "angle": "medial_pivot",
@@ -198,7 +209,7 @@ params_inflated_dorsal_lateral_medial_ventral = {
             },
         },
         {
-            "extent": [0.5, .50, 0.5, .30],
+            "extent": [0.5, 0.50, 0.5, 0.30],
             "view": {
                 "hemisphere": "right",
                 "angle": "lateral_pivot",
@@ -206,7 +217,7 @@ params_inflated_dorsal_lateral_medial_ventral = {
             },
         },
         {
-            "extent": [0.5, .80, 0.5, .20],
+            "extent": [0.5, 0.80, 0.5, 0.20],
             "view": {
                 "hemisphere": "right",
                 "angle": "top_pivot",
