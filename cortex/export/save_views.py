98e288e8d965793f098ca408c98d32974a331587
Matteo Visconti di Oleggio Castello <mvdoc@berkeley.edu>
2021-06-25 17:28:07 -0700

    MNT black save_views.py

--- cortex/export/save_views.py
+++ cortex/export/save_views.py
@@ -6,11 +6,16 @@ import cortex
 file_pattern = "{base}_{view}_{surface}.png"
 
 
-def save_3d_views(volume, base_name='fig', list_angles=['lateral_pivot'],
-                  list_surfaces=['inflated'],
-                  viewer_params=dict(labels_visible=[],
-                                     overlays_visible=['rois']),
-                  size=(1024 * 4, 768 * 4), trim=True, sleep=10):
+def save_3d_views(
+    volume,
+    base_name="fig",
+    list_angles=["lateral_pivot"],
+    list_surfaces=["inflated"],
+    viewer_params=dict(labels_visible=[], overlays_visible=["rois"]),
+    size=(1024 * 4, 768 * 4),
+    trim=True,
+    sleep=10,
+):
     """Saves 3D views of `volume` under multiple specifications.
 
     Needs to be run on a system with a display (will launch webgl viewer).
@@ -66,9 +71,9 @@ def save_3d_views(volume, base_name='fig', list_angles=['lateral_pivot'],
     file_names = []
     for view, surface in zip(list_angles, list_surfaces):
         if isinstance(view, str):
-            if view == 'flatmap' or surface == 'flatmap':
+            if view == "flatmap" or surface == "flatmap":
                 # force flatmap correspondence
-                view = surface = 'flatmap'
+                view = surface = "flatmap"
             view_params = angle_view_params[view]
         else:
             view_params = view
@@ -89,17 +94,16 @@ def save_3d_views(volume, base_name='fig', list_angles=['lateral_pivot'],
         # wait for the view to have changed
         for _ in range(100):
             for k, v in this_view_params.items():
-                k = k.format(subject=volume.subject) if '{subject}' in k else k
+                k = k.format(subject=volume.subject) if "{subject}" in k else k
                 if handle.ui.get(k)[0] != v:
-                    print('waiting for', k, handle.ui.get(k)[0], '->', v)
+                    print("waiting for", k, handle.ui.get(k)[0], "->", v)
                     time.sleep(0.1)
                     continue
             break
         time.sleep(0.1)
 
         # Save image, store file_name
-        file_name = file_pattern.format(base=base_name, view=view,
-                                        surface=surface)
+        file_name = file_pattern.format(base=base_name, view=view, surface=surface)
         file_names.append(file_name)
         handle.getImage(file_name, size)
 
@@ -112,6 +116,7 @@ def save_3d_views(volume, base_name='fig', list_angles=['lateral_pivot'],
         if trim:
             try:
                 import subprocess
+
                 subprocess.call(["convert", "-trim", file_name, file_name])
             except Exception as e:
                 print(str(e))
@@ -123,94 +128,66 @@ def save_3d_views(volume, base_name='fig', list_angles=['lateral_pivot'],
         handle.server.stop()
     except Exception as e:
         print(str(e))
-        print('Could not close viewer.')
+        print("Could not close viewer.")
 
     return file_names
 
 
 default_view_params = {
-    'camera.azimuth': 45,
-    'camera.altitude': 75,
-    'camera.target': [0, 0, 0],
-    'surface.{subject}.unfold': 0,
-    'surface.{subject}.pivot': 0,
-    'surface.{subject}.shift': 0,
-    'surface.{subject}.specularity': 0,
+    "camera.azimuth": 45,
+    "camera.altitude": 75,
+    "camera.target": [0, 0, 0],
+    "surface.{subject}.unfold": 0,
+    "surface.{subject}.pivot": 0,
+    "surface.{subject}.shift": 0,
+    "surface.{subject}.specularity": 0,
 }
 
 angle_view_params = {
-    'left': {
-        'camera.azimuth': 90,
-        'camera.altitude': 90,
+    "left": {"camera.azimuth": 90, "camera.altitude": 90,},
+    "right": {"camera.azimuth": 270, "camera.altitude": 90,},
+    "front": {"camera.azimuth": 0, "camera.altitude": 90,},
+    "back": {"camera.azimuth": 180, "camera.altitude": 90,},
+    "top": {"camera.azimuth": 180, "camera.altitude": 0,},
+    "bottom": {"camera.azimuth": 0, "camera.altitude": 180,},
+    "flatmap": {
+        "camera.azimuth": 180,
+        "camera.altitude": 0,
+        "surface.{subject}.pivot": 180,
+        "surface.{subject}.shift": 0,
     },
-    'right': {
-        'camera.azimuth': 270,
-        'camera.altitude': 90,
+    "medial_pivot": {
+        "camera.azimuth": 0,
+        "camera.altitude": 90,
+        "surface.{subject}.pivot": 180,
+        "surface.{subject}.shift": 10,
     },
-    'front': {
-        'camera.azimuth': 0,
-        'camera.altitude': 90,
+    "lateral_pivot": {
+        "camera.azimuth": 180,
+        "camera.altitude": 90,
+        "surface.{subject}.pivot": 180,
+        "surface.{subject}.shift": 10,
     },
-    'back': {
-        'camera.azimuth': 180,
-        'camera.altitude': 90,
+    "bottom_pivot": {
+        "camera.azimuth": 180,
+        "camera.altitude": 180,
+        "camera.target": [0, -100, 0],
+        "surface.{subject}.pivot": 180,
+        "surface.{subject}.shift": 10,
     },
-    'top': {
-        'camera.azimuth': 180,
-        'camera.altitude': 0,
-    },
-    'bottom': {
-        'camera.azimuth': 0,
-        'camera.altitude': 180,
-    },
-    'flatmap': {
-        'camera.azimuth': 180,
-        'camera.altitude': 0,
-        'surface.{subject}.pivot': 180,
-        'surface.{subject}.shift': 0,
-    },
-    'medial_pivot': {
-        'camera.azimuth': 0,
-        'camera.altitude': 90,
-        'surface.{subject}.pivot': 180,
-        'surface.{subject}.shift': 10,
-    },
-    'lateral_pivot': {
-        'camera.azimuth': 180,
-        'camera.altitude': 90,
-        'surface.{subject}.pivot': 180,
-        'surface.{subject}.shift': 10,
-    },
-    'bottom_pivot': {
-        'camera.azimuth': 180,
-        'camera.altitude': 180,
-        'camera.target': [0, -100, 0],
-        'surface.{subject}.pivot': 180,
-        'surface.{subject}.shift': 10,
-    },
-    'top_pivot': {
-        'camera.azimuth': 180,
-        'camera.altitude': 0,
-        'camera.target': [0, -100, 0],
-        'surface.{subject}.pivot': 180,
-        'surface.{subject}.shift': 10,
+    "top_pivot": {
+        "camera.azimuth": 180,
+        "camera.altitude": 0,
+        "camera.target": [0, -100, 0],
+        "surface.{subject}.pivot": 180,
+        "surface.{subject}.shift": 10,
     },
 }
 
 unfold_view_params = {
-    'fiducial': {
-        'surface.{subject}.unfold': 0,
-    },
-    'inflated_less': {
-        'surface.{subject}.unfold': 0.25,
-    },
-    'inflated': {
-        'surface.{subject}.unfold': 0.5,
-    },
-    'inflated_cut': {
-        'surface.{subject}.unfold': 0.501,
-    },
-    'flatmap': {
-        'surface.{subject}.unfold': 1,
-    },
+    "fiducial": {"surface.{subject}.unfold": 0,},
+    "inflated_less": {"surface.{subject}.unfold": 0.25,},
+    "inflated": {"surface.{subject}.unfold": 0.5,},
+    "inflated_cut": {"surface.{subject}.unfold": 0.501,},
+    "flatmap": {"surface.{subject}.unfold": 1,},
 }
