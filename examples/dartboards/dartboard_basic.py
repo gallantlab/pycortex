c12d26bbd376686a624e0131b7601add7004a6ff
Mark Lescroart <mark.lescroart@gmail.com>
2023-11-01 14:28:24 -0700

    ENH: documentation added, examples modified

--- examples/dartboards/dartboard_basic.py
+++ examples/dartboards/dartboard_basic.py
@@ -1,4 +1,8 @@
-# Basic dartboard pair
+"""Minimal demo of dartboard (radial grid bin) plot.
+
+Note that this type of radial grid averaging is best
+suited for regions for which there is some hypothesis about
+organization centered on a functionally or anatomically defined point in the cortex. This demo uses """
 import cortex
 
 subject = 'S1'
@@ -10,12 +14,22 @@ subject = 'S1'
 # vx = vol.map()
 vx = cortex.db.get_surfinfo(subject, type='curvature')
 
-center = 'M1H'
+# Create a dartboard centered on M1H (primary motor cortex hand representation),
+# anchored to FEF (frontal eye fields above) anterior,
+# M1F (primary motor cortex foot representation) superior,
+# S1H (sensorymotor cortex hand representation) posterior,
+# and M1M (primary motor cortex mouth representation) inferior. 
+# This should show the central sulcus going through the radial grid.
+dartboard_spec = dict(
+    center = 'M1H',
+    anchors = [('FEF', 'centroid'),
+            ('M1F', 'centroid'),
+            ('S1H', 'centroid'),
+            ('M1M', 'centroid'),
+            ],
+    n_angles=16,
+    n_eccentricities=8,
+    max_radii=(50,50),
+)
 
-anchors = [('S1H', 'centroid'),
-           ('M1F', 'centroid'),
-           ('FEF', 'centroid'),
-           ('M1M', 'centroid'),
-           ]
-
-out_m1h = cortex.dartboards.get_dartboard_data(vx, center, anchors)
+axs = cortex.dartboards.show_dartboard_pair(vx, **dartboard_spec)
