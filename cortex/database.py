20dc692e8b7254e3b6b667d23d5ed2b5f0c6c8ac
Aditya Vaidya <kroq-gar78@users.noreply.github.com>
2025-04-02 00:09:06 -0500

    Add types to Database

--- cortex/database.py
+++ cortex/database.py
@@ -1,6 +1,7 @@
 """
 Contains a singleton object `db` of type `Database` which allows easy access to surface files, anatomical images, and transforms that are stored in the pycortex filestore.
 """
+from __future__ import annotations
 import copy
 import functools
 import glob
@@ -12,12 +13,25 @@ import tempfile
 import warnings
 from builtins import input
 from hashlib import sha1
+from typing import Optional, TypedDict
 
 import numpy as np
 
 from . import options
 
 default_filestore = options.config.get('basic', 'filestore')
+
+class PathsType(TypedDict):
+    surfs: dict[str, dict[str, str]]
+    xfms: list[str]
+    xfmdir: str
+    anats: str
+    surfinfo: str
+    masks: str
+    rois: str
+    overlays: str
+    views: list[str]
+    surf2surf: str
 
 
 def _memo(fn):
@@ -151,10 +165,10 @@ class Database(object):
     ----------
     This database object dynamically generates handles to all subjects within the filestore.
     """
-    def __init__(self, filestore=default_filestore):
+    def __init__(self, filestore: str=default_filestore):
         self.filestore = filestore
-        self._subjects = None
-        self.auxfile = None
+        self._subjects: Optional[dict[str, SubjectDB]] = None
+        self.auxfile: Optional[Database] = None
     
     def __repr__(self):
         subjs = "\n   ".join(sorted(self.subjects.keys()))
@@ -435,7 +449,7 @@ class Database(object):
         with open(fname, "w") as fp:
             json.dump(jsdict, fp, sort_keys=True, indent=4)
     
-    def get_xfm(self, subject, name, xfmtype="coord"):
+    def get_xfm(self, subject, name, xfmtype="coord") -> 'Transform':
         """Retrieves a transform from the filestore
 
         Parameters
@@ -465,7 +479,7 @@ class Database(object):
         return Transform(xfmdict[xfmtype], reference)
 
     @_memo
-    def get_surf(self, subject, type, hemisphere="both", merge=False, nudge=False):
+    def get_surf(self, subject, type: str, hemisphere="both", merge=False, nudge=False):
         '''Return the surface pair for the given subject, surface type, and hemisphere.
 
         Parameters
@@ -554,6 +568,7 @@ class Database(object):
         try:
             import nibabel
             nib = nibabel.load(fname)
+            #reveal_type(nib.get_fdata().T != 0)
             return nib.get_fdata().T != 0
         except IOError:
             print('Mask not found, generating...')
@@ -668,7 +683,7 @@ class Database(object):
         if self.subjects[subject]._warning is not None:
             warnings.warn(self.subjects[subject]._warning)
 
-        surfs = dict()
+        surfs: dict[str, dict[str, str]] = dict()
         for surf in os.listdir(surfpath):
             ssurf = os.path.splitext(surf)[0].split('_')
             name = '_'.join(ssurf[:-1])
@@ -683,7 +698,7 @@ class Database(object):
             os.makedirs(viewsdir)
         views = os.listdir(viewsdir)
 
-        filenames = dict(
+        filenames = PathsType(
             surfs=surfs,
             xfms=sorted(os.listdir(os.path.join(self.filestore, subject, "transforms"))),
             xfmdir=os.path.join(self.filestore, subject, "transforms", "{xfmname}", "matrices.xfm"),
