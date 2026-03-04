98475a7912aa65bdad63647a0cb4e7052f6e72db
Aditya Vaidya <kroq-gar78@users.noreply.github.com>
2026-03-04 03:05:08 -0800

    Hail mary to get Database.get_surf working

--- cortex/database.py
+++ cortex/database.py
@@ -482,8 +482,9 @@ class Database(object):
             xfmdict = json.load(f)
         return Transform(xfmdict[xfmtype], reference)
 
+    # TODO: forcing '*' WILL cause issues. Look for all instances of merge=True !
     @overload
-    def get_surf(self, subject: str, type: str, hemisphere: Literal['both'], merge: Literal[True], nudge: bool=False) -> Tuple[npt.NDArray[np.floating], npt.NDArray[np.integer]]:
+    def get_surf(self, subject: str, type: str, hemisphere: Literal['both']='both', *, merge: Literal[True], nudge: bool=False) -> Tuple[npt.NDArray[np.floating], npt.NDArray[np.integer]]:
         ...
 
     @overload
