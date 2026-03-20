8ab476fe5ada49dcb763c699b04ea8c43bf293b3
Aditya Vaidya <kroq-gar78@users.noreply.github.com>
2026-03-19 18:15:04 -0700

    Export vars from .export

--- cortex/export/__init__.py
+++ cortex/export/__init__.py
@@ -1,6 +1,6 @@
-from .save_views import save_3d_views
+from .save_views import save_3d_views, ViewParams
 from .headless import headless_viewer
-from .panels import plot_panels
+from .panels import plot_panels, PanelView, PanelParams
 from ._default_params import (
     params_inflatedless_lateral_medial_ventral,
     params_flatmap_lateral_medial,
@@ -11,8 +11,11 @@ from ._default_params import (
 
 __all__ = [
     "save_3d_views",
+    "ViewParams",
     "headless_viewer",
     "plot_panels",
+    "PanelView",
+    "PanelParams",
     "params_flatmap_lateral_medial",
     "params_occipital_triple_view",
     "params_inflatedless_lateral_medial_ventral",
